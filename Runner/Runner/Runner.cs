323e7ece434cd8f7b4ad3a4b9d16f2f858f4ec9b
Yegor Lindberg <yegor.lindberg@gmail.com>
2019-10-13 16:41:00 +0300

    Внедрение логики хождения по таблице.

--- Runner/Runner/Runner.cs
+++ Runner/Runner/Runner.cs
@@ -20,48 +20,48 @@ namespace Runner
 
     class Runner
     {
+        private string strSeparator = ",";
         private Stack enterChain = new Stack();
         private Stack tableKey = new Stack();
         private List<Table> resultTable = new List<Table>(); //вынес в глобальную переменную, чтобы слишком часто не передавать по значению(экономим немного памяти)
+        List<Dictionary<string, List<string>>> rules; //правила берем из slr, они там уже есть.
+
+
+        public Runner(List<Dictionary<string, List<string>>> _rules)
+        {
+            rules = _rules;
+        }
 
         //входная цепочка может состоять из подстрок и разделяется пробелами.
         //например: "abc" => "a b c d,e" |  "lot fok top lol,ik"
-        public bool Convolution(List<Table> table, String enterString) //return true if success.
+        public bool Convolution(List<Table> table, string enterString) //return true if success.
         {
             resultTable = table;
             List<string> enterStrArr = new List<string> (enterString.Split(' '));
-            int ctr;
-            for (ctr = 0; ctr < enterStrArr.Count; ctr++)
-            {
-                int? i = GetIndexFromTableWith(enterStrArr[ctr]);
-                if (i != null) {
-                    enterChain.Push(enterStrArr[ctr]);
-                    
-                } 
-                else
-                {
-                    //fatalErr
-                }
-                
-            }
+            
+            ProcessChain(enterStrArr);
+            
             return true;
         }
 
-        //["id1", "id2"] -> "id1,id2";
-        //далее происходит сравнение строк.
         bool IsEqualKeys(List<string> key, string value)
         {
-            string keyVal = "";
-            foreach (string id in key)
+            return MakeStringFromList(key) == value;
+        }
+
+        //["id1", "id2"] -> "id1,id2";
+        string MakeStringFromList(List<string> valueOfColumn)
+        {
+            string val = "";
+            foreach (string elem in valueOfColumn)
             {
-                if (keyVal != "") { keyVal += "," + id; }
-                else { keyVal = id; }
+                val = (val == "") ? elem : (val + strSeparator + elem);
             }
-            return keyVal == value;
+            return val;
         }
 
 
-        int? GetIndexFromTableWith(string value)
+        int? GetKeyIndexFromTableWith(string value)
         {
             int ctr = 0;
             while (!IsEqualKeys(resultTable[ctr].key, value))
@@ -70,6 +70,54 @@ namespace Runner
                 else { return null; }
             }
             return ctr;
+        }
+
+        int? GetIndexFromValue(string value)
+        {
+            int ctr = 0;
+            while (resultTable[0].value[ctr].columnOfTable != value)
+            {
+                if (resultTable.Count > (ctr + 1)) { ctr++; }
+                else { return null; }
+            }
+            return ctr;
+        }
+
+        void ProcessChain(List<string> chain)
+        {
+            int ctr;
+            for (ctr = 0; ctr < chain.Count; ctr++)
+            {
+                int? i = GetKeyIndexFromTableWith(chain[ctr]);
+                if (i == null)
+                {
+                    //fatalErr
+                    Console.WriteLine("\n--- [Fatal Error]: Element \"", chain[ctr], "\" not found.\n");
+                    break;
+                }
+                enterChain.Push(chain[ctr]);
+                if (ctr + 1 < chain.Count)
+                {
+                    int? indexOfNextVal = GetIndexFromValue(chain[ctr + 1]);
+                    int idNext = indexOfNextVal ?? -1;
+                    if ((MakeStringFromList(resultTable[ctr].value[idNext].valueOfColumn) == "RETURN") 
+                        || (MakeStringFromList(resultTable[ctr].value[resultTable[ctr].value.Count].valueOfColumn) == "RETURN"))
+                    {
+                        TryToConvolutionInRule(ctr);
+                    }
+                }
+                else if (MakeStringFromList(resultTable[ctr].value[resultTable[ctr].value.Count].valueOfColumn) == "RETURN") //подумать над концовкой
+                {
+                    TryToConvolutionInRule(ctr);
+                }
+            }
+        }
+
+        
+
+        void TryToConvolutionInRule(int numberOfRule)
+        {
+
         }
     }
 }
