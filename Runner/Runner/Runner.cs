d2b263c3a7854d34543b42636b3960ccc76e1401
Yegor Lindberg <yegor.lindberg@gmail.com>
2019-10-13 12:10:01 +0300

    Заложил основу.
    Нужно подумать на счет стэков, возможно, Нехорошкова была права и нужен лишь один, так было бы проще. Однако, для этого надо знать, в каком правиле находится каждый из ключей.

--- Runner/Runner/Runner.cs
+++ Runner/Runner/Runner.cs
@@ -20,19 +20,56 @@ namespace Runner
 
     class Runner
     {
-        Stack enterChain = new Stack();
-        Stack tableKey = new Stack();
+        private Stack enterChain = new Stack();
+        private Stack tableKey = new Stack();
+        private List<Table> resultTable = new List<Table>(); //вынес в глобальную переменную, чтобы слишком часто не передавать по значению(экономим немного памяти)
 
         //входная цепочка может состоять из подстрок и разделяется пробелами.
-        //например: "abc" => "a b c" |  "lot fok top"
-        void runner(List<Table> table, String enterString)
+        //например: "abc" => "a b c d,e" |  "lot fok top lol,ik"
+        public bool Convolution(List<Table> table, String enterString) //return true if success.
         {
-            string [] enterStrArr = enterString.Split(' ');
-            foreach (string element in enterStrArr)
+            resultTable = table;
+            List<string> enterStrArr = new List<string> (enterString.Split(' '));
+            int ctr;
+            for (ctr = 0; ctr < enterStrArr.Count; ctr++)
             {
+                int? i = GetIndexFromTableWith(enterStrArr[ctr]);
+                if (i != null) {
+                    enterChain.Push(enterStrArr[ctr]);
+                    
+                } 
+                else
+                {
+                    //fatalErr
+                }
                 
             }
+            return true;
         }
 
+        //["id1", "id2"] -> "id1,id2";
+        //далее происходит сравнение строк.
+        bool IsEqualKeys(List<string> key, string value)
+        {
+            string keyVal = "";
+            foreach (string id in key)
+            {
+                if (keyVal != "") { keyVal += "," + id; }
+                else { keyVal = id; }
+            }
+            return keyVal == value;
+        }
+
+
+        int? GetIndexFromTableWith(string value)
+        {
+            int ctr = 0;
+            while (!IsEqualKeys(resultTable[ctr].key, value))
+            {
+                if (resultTable.Count > (ctr + 1)) { ctr++; } 
+                else { return null; }
+            }
+            return ctr;
+        }
     }
 }
