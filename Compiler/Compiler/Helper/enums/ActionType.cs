5f79128ae2f2cbfe8edc3cbc48360e0f79283132
fjodor-rybakov <fedryb@mail.ru>
2020-01-26 11:56:32 +0300

    add new action

--- Compiler/Compiler/Helper/enums/ActionType.cs
+++ Compiler/Compiler/Helper/enums/ActionType.cs
@@ -7,6 +7,13 @@ namespace Compiler.Helper.enums
         Multiplication,
         Division,
         DefineLeaf,
-        DefineUnaryMinus
+        DefineUnaryMinus,
+        DefineVar,
+        DefineWrite,
+        DefineWriteLine,
+        DefileRead,
+        DefineIfCondition,
+        DefineIfElseCondition,
+        DefineWhile
     }
 }
