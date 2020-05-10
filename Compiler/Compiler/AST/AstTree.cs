e6ba76e16ce327052d890df1acf7880f64862bfd
fjodor-rybakov <fedryb@mail.ru>
2020-05-10 22:41:53 +0300

    add unique key for print

--- Compiler/Compiler/AST/AstTree.cs
+++ Compiler/Compiler/AST/AstTree.cs
@@ -9,6 +9,7 @@ namespace Compiler.AST
     public class AstTree
     {
         private readonly Stack<TreeNode> _tree = new Stack<TreeNode>();
+        private int uniqueKey = 0;
 
         public void CreateNode(ColumnOfReestr registry, string value, int countRemoveElems)
         {
@@ -16,63 +17,63 @@ namespace Compiler.AST
             {
                 case nameof(ActionType.DefineLeaf):
                 {
-                    _tree.Push(new TreeNode(TermType.Int, ActionType.DefineLeaf, value, null));
+                    _tree.Push(new TreeNode(TermType.Int, ActionType.DefineLeaf, value, null, uniqueKey++));
                     break;
                 }
                 case nameof(ActionType.DefineUnaryMinus):
                 {
                     var tempList = new List<TreeNode>();
                     for (var i = 0; i < countRemoveElems - 1; i++) tempList.Add(_tree.Pop());
-                    _tree.Push(new TreeNode(TermType.Minis, ActionType.DefineUnaryMinus, value, tempList));
+                    _tree.Push(new TreeNode(TermType.Minis, ActionType.DefineUnaryMinus, value, tempList, uniqueKey++));
                     break;
                 }
                 case nameof(ActionType.Sum):
                 {
                     var tempList = new List<TreeNode>();
                     for (var i = 0; i < countRemoveElems; i++) tempList.Add(_tree.Pop());
-                    _tree.Push(new TreeNode(TermType.Plus, ActionType.Sum, "+", tempList));
+                    _tree.Push(new TreeNode(TermType.Plus, ActionType.Sum, "+", tempList, uniqueKey++));
                     break;
                 }
                 case nameof(ActionType.Subtract):
                 {
                     var tempList = new List<TreeNode>();
                     for (var i = 0; i < countRemoveElems; i++) tempList.Add(_tree.Pop());
-                    _tree.Push(new TreeNode(TermType.Minis, ActionType.Subtract, "-", tempList));
+                    _tree.Push(new TreeNode(TermType.Minis, ActionType.Subtract, "-", tempList, uniqueKey++));
                     break;
                 }
                 case nameof(ActionType.Multiplication):
                 {
                     var tempList = new List<TreeNode>();
                     for (var i = 0; i < countRemoveElems; i++) tempList.Add(_tree.Pop());
-                    _tree.Push(new TreeNode(TermType.Multiple, ActionType.Multiplication, "*", tempList));
+                    _tree.Push(new TreeNode(TermType.Multiple, ActionType.Multiplication, "*", tempList, uniqueKey++));
                     break;
                 }
                 case nameof(ActionType.Division):
                 {
                     var tempList = new List<TreeNode>();
                     for (var i = 0; i < countRemoveElems; i++) tempList.Add(_tree.Pop());
-                    _tree.Push(new TreeNode(TermType.Division, ActionType.Division, "/", tempList));
+                    _tree.Push(new TreeNode(TermType.Division, ActionType.Division, "/", tempList, uniqueKey++));
                     break;
                 }
                 case nameof(ActionType.DefineBody):
                 {
                     var tempList = new List<TreeNode>();
                     for (var i = 0; i < countRemoveElems; i++) tempList.Add(_tree.Pop());
-                    _tree.Push(new TreeNode(TermType.Multiple, ActionType.DefineBody, value, tempList));
+                    _tree.Push(new TreeNode(TermType.Multiple, ActionType.DefineBody, value, tempList, uniqueKey++));
                     break;
                 }
                 case nameof(ActionType.DefineVarStatement):
                 {
                     var tempList = new List<TreeNode>();
                     for (var i = 0; i < countRemoveElems; i++) tempList.Add(_tree.Pop());
-                    _tree.Push(new TreeNode(TermType.Multiple, ActionType.DefineVarStatement, value, tempList));
+                    _tree.Push(new TreeNode(TermType.Multiple, ActionType.DefineVarStatement, value, tempList, uniqueKey++));
                     break;
                 }
                 case nameof(ActionType.DefineVarDeclaration):
                 {
                     var tempList = new List<TreeNode>();
                     for (var i = 0; i < countRemoveElems; i++) tempList.Add(_tree.Pop());
-                    _tree.Push(new TreeNode(TermType.Multiple, ActionType.DefineVarDeclaration, value, tempList));
+                    _tree.Push(new TreeNode(TermType.Multiple, ActionType.DefineVarDeclaration, value, tempList, uniqueKey++));
                     break;
                 }
                 case null:
@@ -109,8 +110,8 @@ namespace Compiler.AST
                 {
                     Traversal(elem);
                 }
-            var childrenListString = node.ChildNodes == null ? "null" : string.Join(", ", node.ChildNodes.Select(item => item.Value));
-            Console.WriteLine($"|{node.Value, -10}|{childrenListString, -20}|{node.ActionType, -20}|");
+            var childrenListString = node.ChildNodes == null ? "null" : string.Join(", ", node.ChildNodes.Select(item => item.Value + '(' + item.UniqueKey + ')'));
+            Console.WriteLine($"|{node.Value + '(' + node.UniqueKey + ')', -10}|{childrenListString, -20}|{node.ActionType, -20}|");
         }
 
         public TreeNode GetTree()
